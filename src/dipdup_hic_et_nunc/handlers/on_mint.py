7886edfa12d84985bda50dcfedca937a6a0dc945
Lev Gorodetskiy <github@droserasprout.space>
2021-04-05 11:33:28 +0300

    Test codegen results validity

--- src/dipdup_hic_et_nunc/handlers/on_mint.py
+++ src/dipdup_hic_et_nunc/handlers/on_mint.py
@@ -1,10 +1,10 @@
 from typing import List
+
 from dipdup.models import HandlerContext, OperationData
-
 from dipdup_hic_et_nunc.models import *
-
 from dipdup_hic_et_nunc.types.KT1Hkg5qeNhfwpKW4fXvq7HGZB9z2EnmCCA9.parameter.mint_OBJKT import MintObjkt
 from dipdup_hic_et_nunc.types.KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton.parameter.mint import Mint
+
 
 async def on_mint(
     mint_OBJKT: HandlerContext[MintObjkt],
--- src/dipdup_hic_et_nunc/handlers/on_transfer.py
+++ src/dipdup_hic_et_nunc/handlers/on_transfer.py
@@ -1,9 +1,9 @@
 from typing import List
+
 from dipdup.models import HandlerContext, OperationData
-
 from dipdup_hic_et_nunc.models import *
-
 from dipdup_hic_et_nunc.types.KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton.parameter.transfer import Transfer
+
 
 async def on_transfer(
     transfer: HandlerContext[Transfer],
