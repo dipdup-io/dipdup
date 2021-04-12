4591c7380de9ce53c5f56c100850848434ed88fa
Lev Gorodetskiy <github@droserasprout.space>
2021-04-12 17:54:09 +0300

    Fix cancel_swap

--- src/demo_hic_et_nunc/handlers/on_cancel_swap.py
+++ src/demo_hic_et_nunc/handlers/on_cancel_swap.py
@@ -7,6 +7,6 @@ async def on_cancel_swap(
     ctx: HandlerContext,
     cancel_swap: OperationContext[CancelSwap],
 ) -> None:
-    swap = await models.Swap.filter(id=cancel_swap.parameter).get()
+    swap = await models.Swap.filter(id=int(cancel_swap.parameter.__root__)).get()
     swap.status = models.SwapStatus.CANCELED
     await swap.save()
