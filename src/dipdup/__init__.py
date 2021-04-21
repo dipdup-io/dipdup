8db6098d96b16329fdcb226e808be76de289af3f
Lev Gorodetskiy <github@droserasprout.space>
2021-04-21 13:10:32 +0300

    Revert "Patch aiorignalrcore to avoid reconnections"
    This reverts commit 3fda559d887a99b79676e7e627e401dd2c9a49c2.

--- src/dipdup/__init__.py
+++ src/dipdup/__init__.py
@@ -1,15 +1,1 @@
-import asyncio
-
-from aiosignalrcore.transport.websockets.reconnection import ConnectionStateChecker  # type: ignore
-
 __version__ = '0.1.0'
-
-
-async def run(self):
-    while self.running:
-        await asyncio.sleep(self.keep_alive_interval)
-        await self.ping_function()
-
-
-# FIXME: https://github.com/mandrewcito/signalrcore/pull/58
-ConnectionStateChecker.run = run
