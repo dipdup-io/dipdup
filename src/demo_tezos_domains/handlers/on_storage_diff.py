5cfabc5ab7a5d6e48a097f2f1b1fd803888f36d3
m-kus <mz@baking-bad.org>
2021-04-23 17:29:09 +0300

    Validate record name

--- src/demo_tezos_domains/handlers/on_storage_diff.py
+++ src/demo_tezos_domains/handlers/on_storage_diff.py
@@ -12,7 +12,12 @@ _logger = logging.getLogger(__name__)
 async def on_storage_diff(storage: NameRegistryStorage) -> None:
     for name, item in storage.store.records.items():
         record_name = bytes.fromhex(name).decode()
+        record_path = record_name.split('.')
         _logger.info(f'Processing `{record_name}`')
+
+        if len(record_path) != int(item.level):
+            _logger.error(f'Invalid record `{record_name}`: expected {item.level} chunks, got {len(record_path)}')
+            return
 
         if item.level == "1":
             await models.TLD.update_or_create(
@@ -26,7 +31,7 @@ async def on_storage_diff(storage: NameRegistryStorage) -> None:
                 await models.Domain.update_or_create(
                     id=record_name,
                     defaults=dict(
-                        tld_id=record_name.split('.')[-1],
+                        tld_id=record_path[-1],
                         owner=item.owner,
                         expiry=storage.store.expiry_map.get(item.expiry_key),
                         token_id=int(item.tzip12_token_id) if item.tzip12_token_id else None
@@ -36,7 +41,7 @@ async def on_storage_diff(storage: NameRegistryStorage) -> None:
             await models.Record.update_or_create(
                 id=record_name,
                 defaults=dict(
-                    domain_id='.'.join(record_name.split('.')[-2:]),
+                    domain_id='.'.join(record_path[-2:]),
                     address=item.address
                 )
             )
