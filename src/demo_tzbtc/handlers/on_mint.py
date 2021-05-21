92c963df32cabaf4c3ca26b172775089cf20fa4f
Michael Zaikin <mz@baking-bad.org>
2021-05-21 16:22:48 +0300

    Quickstart demo (#48)

---
+++ src/demo_tzbtc/handlers/on_mint.py
@@ -0,0 +1,16 @@
+from decimal import Decimal
+from typing import Optional
+
+import demo_tzbtc.models as models
+from demo_tzbtc.handlers.on_balance_update import on_balance_update
+from demo_tzbtc.types.tzbtc.parameter.mint import MintParameter
+from demo_tzbtc.types.tzbtc.storage import TzbtcStorage
+from dipdup.models import OperationData, OperationHandlerContext, OriginationContext, TransactionContext
+
+
+async def on_mint(
+    ctx: OperationHandlerContext,
+    mint: TransactionContext[MintParameter, TzbtcStorage],
+) -> None:
+    amount = Decimal(mint.parameter.value) / (10 ** 8)
+    await on_balance_update(address=mint.parameter.to, balance_update=amount, timestamp=mint.data.timestamp)
