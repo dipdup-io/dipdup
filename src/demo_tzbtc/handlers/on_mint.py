314e1af9bacd7419a4622e26dfa15fa6b285e784
m-kus <mz@baking-bad.org>
2021-05-21 15:37:27 +0300

    Linter fix

--- src/demo_tzbtc/handlers/on_mint.py
+++ src/demo_tzbtc/handlers/on_mint.py
@@ -1,13 +1,11 @@
-from typing import Optional
 from decimal import Decimal
-
-from dipdup.models import OperationData, OperationHandlerContext, OriginationContext, TransactionContext
+from typing import Optional
 
 import demo_tzbtc.models as models
-
+from demo_tzbtc.handlers.on_balance_update import on_balance_update
 from demo_tzbtc.types.tzbtc.parameter.mint import MintParameter
 from demo_tzbtc.types.tzbtc.storage import TzbtcStorage
-from demo_tzbtc.handlers.on_balance_update import on_balance_update
+from dipdup.models import OperationData, OperationHandlerContext, OriginationContext, TransactionContext
 
 
 async def on_mint(
@@ -15,6 +13,4 @@ async def on_mint(
     mint: TransactionContext[MintParameter, TzbtcStorage],
 ) -> None:
     amount = Decimal(mint.parameter.value) / (10 ** 8)
-    await on_balance_update(address=mint.parameter.to,
-                            balance_update=amount,
-                            timestamp=mint.data.timestamp)
+    await on_balance_update(address=mint.parameter.to, balance_update=amount, timestamp=mint.data.timestamp)
--- src/demo_tzbtc/handlers/on_transfer.py
+++ src/demo_tzbtc/handlers/on_transfer.py
@@ -1,13 +1,11 @@
-from typing import Optional
 from decimal import Decimal
-
-from dipdup.models import OperationData, OperationHandlerContext, OriginationContext, TransactionContext
+from typing import Optional
 
 import demo_tzbtc.models as models
-
+from demo_tzbtc.handlers.on_balance_update import on_balance_update
 from demo_tzbtc.types.tzbtc.parameter.transfer import TransferParameter
 from demo_tzbtc.types.tzbtc.storage import TzbtcStorage
-from demo_tzbtc.handlers.on_balance_update import on_balance_update
+from dipdup.models import OperationData, OperationHandlerContext, OriginationContext, TransactionContext
 
 
 async def on_transfer(
@@ -17,9 +15,5 @@ async def on_transfer(
     if transfer.parameter.from_ == transfer.parameter.to:
         return
     amount = Decimal(transfer.parameter.value) / (10 ** 8)
-    await on_balance_update(address=transfer.parameter.from_,
-                            balance_update=-amount,
-                            timestamp=transfer.data.timestamp)
-    await on_balance_update(address=transfer.parameter.to,
-                            balance_update=amount,
-                            timestamp=transfer.data.timestamp)
+    await on_balance_update(address=transfer.parameter.from_, balance_update=-amount, timestamp=transfer.data.timestamp)
+    await on_balance_update(address=transfer.parameter.to, balance_update=amount, timestamp=transfer.data.timestamp)
