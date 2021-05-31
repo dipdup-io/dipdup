1a1aff81ce1904989e9921e2d4bdfb18fbc99ec5
Lev Gorodetskiy <github@droserasprout.space>
2021-05-31 17:12:35 +0300

    Cleanup

--- src/demo_dex/types/token_fa12/storage.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Extra
-
-
-# NOTE: This indexer does not use token storage which may vary from one contract to another (for ex. tzBTC)
-class TokenFa12Storage(BaseModel):
-    pass
--- src/demo_dex/types/token_fa2/storage.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-# NOTE: This indexer does not use token storage which may vary from one contract to another (for ex. tzBTC)
-class TokenFa2Storage(BaseModel):
-    pass
