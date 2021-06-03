dfbb83ebceb32cb86b41a71bc127c0c8d9976cec
Lev Gorodetskiy <github@droserasprout.space>
2021-06-03 14:05:58 +0300

    Reindex even on a single level rollback

--- src/demo_hic_et_nunc/handlers/on_rollback.py
+++ src/demo_hic_et_nunc/handlers/on_rollback.py
@@ -9,7 +9,5 @@ async def on_rollback(
     from_level: int,
     to_level: int,
 ) -> None:
-    if from_level - to_level == 1:
-        return
     _logger.warning('Rollback event received, reindexing')
     await reindex()
