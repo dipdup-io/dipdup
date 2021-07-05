39d5f8ee578a975b6a85f2e7121671e3f03c607a
Lev Gorodetskiy <github@droserasprout.space>
2021-07-05 10:20:14 +0300

    Retry failed requests in DatasourceRequestProxy

--- src/dipdup/datasources/proxy.py
+++ src/dipdup/datasources/proxy.py
@@ -1,3 +1,4 @@
+import asyncio
 import hashlib
 import logging
 import pickle
@@ -9,13 +10,39 @@ from fcache.cache import FileCache  # type: ignore
 
 from dipdup.utils import http_request
 
+DEFAULT_RETRY_COUNT = 3
+DEFAULT_RETRY_SLEEP = 1
+
 
 class DatasourceRequestProxy:
-    def __init__(self, cache: bool = False, ratelimiter: Optional[AsyncLimiter] = None) -> None:
+    """Wrapper for datasource HTTP requests.
+
+    Covers caching, retrying failed requests and ratelimiting"""
+
+    def __init__(
+        self,
+        cache: bool = False,
+        ratelimiter: Optional[AsyncLimiter] = None,
+        retry_count: int = DEFAULT_RETRY_COUNT,
+        retry_sleep: int = DEFAULT_RETRY_SLEEP,
+    ) -> None:
         self._logger = logging.getLogger(__name__)
         self._cache = FileCache('dipdup', flag='cs') if cache else None
         self._ratelimiter = ratelimiter
+        self._retry_count = retry_count
+        self._retry_sleep = retry_sleep
         self._session = aiohttp.ClientSession()
+
+    async def _wrapped_request(self, method: str, **kwargs):
+        for attempt in range(self._retry_count):
+            self._logger.debug('Datasource request attempt %s/%s', attempt + 1, self._retry_count)
+            try:
+                return await http_request(self._session, method, **kwargs)
+            except aiohttp.ClientConnectionError as e:
+                if attempt + 1 == self._retry_count:
+                    raise e
+                self._logger.warning('Datasource request failed: %s', e)
+                await asyncio.sleep(self._retry_sleep)
 
     async def http_request(self, method: str, skip_cache: bool = False, weight: int = 1, **kwargs):
         if self._cache is not None and not skip_cache:
@@ -25,21 +52,13 @@ class DatasourceRequestProxy:
             except KeyError:
                 if self._ratelimiter:
                     await self._ratelimiter.acquire(weight)
-                response = await http_request(
-                    session=self._session,
-                    method=method,
-                    **kwargs,
-                )
+                response = await self._wrapped_request(method, **kwargs)
                 self._cache[key] = response
                 return response
         else:
             if self._ratelimiter:
                 await self._ratelimiter.acquire(weight)
-            response = await http_request(
-                session=self._session,
-                method=method,
-                **kwargs,
-            )
+            response = await self._wrapped_request(method, **kwargs)
             return response
 
     async def close_session(self) -> None:
