e39931ec583fcecd8016200f88f265052604df24
Lev Gorodetskiy <github@droserasprout.space>
2021-07-09 10:00:20 +0300

    Fix BCD datasource `get_tokens` method

--- src/dipdup/datasources/bcd/datasource.py
+++ src/dipdup/datasources/bcd/datasource.py
@@ -3,11 +3,11 @@ from typing import Any, Dict, List
 
 from dipdup.datasources.proxy import DatasourceRequestProxy
 
-Address = str
+TOKENS_REQUEST_LIMIT = 10
 
 
 class BcdDatasource:
-    def __init__(self, url: str, network: str, proxy=DatasourceRequestProxy()) -> None:
+    def __init__(self, url: str, network: str, proxy: DatasourceRequestProxy) -> None:
         self._url = url.rstrip('/')
         self._network = network
         self._proxy = proxy
@@ -22,8 +22,21 @@ class BcdDatasource:
     async def resync(self) -> None:
         pass
 
-    async def get_tokens(self, address: Address) -> List[Dict[str, Any]]:
+    async def get_tokens(self, address: str) -> List[Dict[str, Any]]:
+        tokens, offset = [], 0
+        while True:
+            tokens_batch = await self._proxy.http_request(
+                'get',
+                url=f'{self._url}/v1/contract/{self._network}/{address}/tokens?offset={offset}',
+            )
+            tokens += tokens_batch
+            offset += TOKENS_REQUEST_LIMIT
+            if len(tokens_batch) < TOKENS_REQUEST_LIMIT:
+                break
+        return tokens
+
+    async def get_token(self, address: str, token_id: int) -> Dict[str, Any]:
         return await self._proxy.http_request(
             'get',
-            url=f'{self._url}/v1/contract/{self._network}/{address}/tokens',
+            url=f'{self._url}/v1/contract/{self._network}/{address}/tokens?token_id={token_id}',
         )
