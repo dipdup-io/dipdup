e3455cb0ddaea939d6c8c02107743b28979787fa
Lev Gorodetskiy <github@droserasprout.space>
2021-08-30 18:09:17 +0300

    Cleanup

--- src/demo_tzcolors/hooks/test.py
+++
@@ -1,8 +0,0 @@
-
-from dipdup.context import HookContext
-from dipdup.exceptions import CallbackNotImplementedError
-
-async def test(
-    ctx: HookContext,
-) -> None:
-    await ctx.execute_sql('test')
