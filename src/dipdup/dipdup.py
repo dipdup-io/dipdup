b04b81f52280e40b4675e3fecf848e3acf81d7db
Lev Gorodetskiy <github@droserasprout.space>
2021-09-24 14:01:02 +0300

    Fix datasource spawn event detection

--- src/dipdup/dipdup.py
+++ src/dipdup/dipdup.py
@@ -74,7 +74,11 @@ class IndexDispatcher:
                     spawn_datasources_event.set()
 
             if start_scheduler_event and not start_scheduler_event.is_set():
-                if self._every_index_is(IndexStatus.REALTIME):
+                # NOTE: Do not check with every_index_is, indexes become REALTIME after first message from WS is received
+                for index in self._indexes.values():
+                    if index.state.level != index.datasource.head.level:
+                        break
+                else:
                     start_scheduler_event.set()
 
     def stop(self) -> None:
