be4b8efc6c16c0c096111b2909dde63d6afbdb73
Lev Gorodetskiy <github@droserasprout.space>
2021-09-28 12:53:20 +0300

    Fix entering sync state

--- src/dipdup/models.py
+++ src/dipdup/models.py
@@ -316,7 +316,6 @@ class Index(Model):
 
         if head:
             self.head = head
-            self.head_id = head.pk if head else None
 
         self.status = status
         await self.save()
