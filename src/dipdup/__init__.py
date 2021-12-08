1bc81b5d23421c48dafd2403add7cd33d2935a40
Lev Gorodetskiy <github@droserasprout.space>
2021-12-08 20:04:46 +0400

    Internal docs (#181)

--- src/dipdup/__init__.py
+++ src/dipdup/__init__.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 __version__ = '3.1.3'
 __spec_version__ = '1.2'
 spec_version_mapping = {
@@ -12,3 +15,5 @@ spec_reindex_mapping = {
     '1.1': True,
     '1.2': True,
 }
+
+sys.path.append(os.getcwd())
