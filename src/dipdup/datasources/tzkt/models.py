d00b367c066686865326a61ee09b17c0b12e3a29
Lev Gorodetskiy <github@droserasprout.space>
2021-12-30 17:54:47 +0400

    Refactoring, docs

--- src/dipdup/datasources/tzkt/models.py
+++ src/dipdup/datasources/tzkt/models.py
@@ -5,11 +5,12 @@ from typing import Any
 from typing import Dict
 from typing import Iterable
 from typing import List
+from typing import Optional
 from typing import Type
 from typing import Union
-from typing import cast
 
 from pydantic.error_wrappers import ValidationError
+from pydantic.fields import FieldInfo
 from typing_extensions import get_args
 from typing_extensions import get_origin
 
@@ -20,41 +21,44 @@ from dipdup.models import StorageType
 IntrospectionError = (KeyError, IndexError, AttributeError)
 
 
-def _get_root_type(storage_type: Type) -> Type:
+def _extract_root_type(storage_type: Type) -> Type:
+    """Extract Pydantic __root__ type"""
     return storage_type.__fields__['__root__'].type_
 
 
 @lru_cache(None)
 def _is_array(storage_type: Type) -> bool:
     """TzKT can return bigmaps as objects or as arrays of key-value objects. Guess it from storage type."""
+    # NOTE: List[...]
     if get_origin(storage_type) == list:
         return True
 
-    try:
-        fields = storage_type.__fields__
-    # NOTE: Not a dataclass
-    except AttributeError:
+    # NOTE: Neither a list not Pydantic model, can't be an array
+    fields: Optional[Dict[str, FieldInfo]] = getattr(storage_type, '__fields__', None)
+    if fields is None:
         return False
 
-    # NOTE: TzKT array (dict actually)
+    # NOTE: An item of TzKT array
     if 'key' in fields and 'value' in fields:
         return True
 
-    # NOTE: Pydantic array
-    try:
-        root_type = _get_root_type(storage_type)
+    # NOTE: Pydantic model with __root__ field, dive into it
+    with suppress(*IntrospectionError):
+        root_type = _extract_root_type(storage_type)
         return _is_array(root_type)  # type: ignore
-    except IntrospectionError:
-        return False
+
+    # NOTE: Something else
+    return False
 
 
 @lru_cache(None)
 def _extract_list_types(storage_type: Type[Any]) -> Iterable[Type[Any]]:
-    # NOTE: Pydantic model with list root
+    """Extract list item types from field type"""
+    # NOTE: Pydantic model with __root__ field
     with suppress(*IntrospectionError):
-        return (_get_root_type(storage_type),)
+        return (_extract_root_type(storage_type),)
 
-    # NOTE: Python list
+    # NOTE: Python list, return all args unpacking unions
     with suppress(*IntrospectionError):
         item_type = get_args(storage_type)[0]
         if get_origin(item_type) == Union:
@@ -67,18 +71,23 @@ def _extract_list_types(storage_type: Type[Any]) -> Iterable[Type[Any]]:
 
 @lru_cache(None)
 def _extract_dict_types(storage_type: Type[Any], key: str) -> Iterable[Type[Any]]:
+    """Extract dict value types from field type"""
+    # NOTE: Regular dict
     if get_origin(storage_type) == dict:
         return (get_args(storage_type)[1],)
 
+    # NOTE: Unpack union args
     if get_origin(storage_type) == Union:
         return get_args(storage_type)
 
+    # NOTE: Pydantic model, find corresponding field and return it's type
     with suppress(*IntrospectionError):
         fields = storage_type.__fields__
         for field in fields.values():
             if key in (field.name, field.alias):
                 return (field.type_,)
 
+    # NOTE: Something else
     return ()
 
 
@@ -98,20 +107,21 @@ def _apply_bigmap_diffs(
     bigmap_diffs: Dict[int, Iterable[Dict[str, Any]]],
     is_array: bool,
 ) -> Union[List[Dict[str, Any]], Dict[str, Any]]:
-    storage: Union[List[Dict[str, Any]], Dict[str, Any]] = [] if is_array else {}
-    for diff in bigmap_diffs.get(bigmap_id, ()):
-        bigmap_key, bigmap_value = diff['content']['key'], diff['content']['value']
-        if is_array:
-            cast(list, storage).append(
-                {
-                    'key': bigmap_key,
-                    'value': bigmap_value,
-                }
-            )
-        else:
-            storage[bigmap_key] = bigmap_value
+    """Apply bigmap diffs to the storage"""
+    diffs = bigmap_diffs.get(bigmap_id, ())
+    diffs_items = ((d['content']['key'], d['content']['value']) for d in diffs)
 
-    return storage
+    if is_array:
+        list_storage: List[Dict[str, Any]] = []
+        for key, value in diffs_items:
+            list_storage.append({'key': key, 'value': value})
+        return list_storage
+
+    else:
+        dict_storage: Dict[str, Any] = {}
+        for key, value in diffs_items:
+            dict_storage[key] = value
+        return dict_storage
 
 
 def _process_storage(
@@ -119,21 +129,21 @@ def _process_storage(
     storage_type: Type[StorageType],
     bigmap_diffs: Dict[int, Iterable[Dict[str, Any]]],
 ) -> Any:
+    """Replace bigmap pointers with actual data from diffs"""
     # NOTE: Bigmap pointer, apply diffs
     if isinstance(storage, int) and storage_type not in (int, bool):
         is_array = _is_array(storage_type)
         storage = _apply_bigmap_diffs(storage, bigmap_diffs, is_array)
 
-    # NOTE: List of something, apply diffs recursively if needed
+    # NOTE: List, process recursively
     elif isinstance(storage, list):
         for i, _ in enumerate(storage):
             for item_type in _extract_list_types(storage_type):
                 with suppress(*IntrospectionError):
                     storage[i] = _process_storage(storage[i], item_type, bigmap_diffs)
 
-    # NOTE: Regular dict, possibly nested: fire up introspection magic
+    # NOTE: Dict, process recursively
     elif isinstance(storage, dict):
-
         for key, value in storage.items():
             for value_type in _extract_dict_types(storage_type, key):
                 with suppress(*IntrospectionError):
