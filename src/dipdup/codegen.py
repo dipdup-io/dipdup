095aba96be221e9a2c2e414e59efbe1371b05739
Lev Gorodetskiy <github@droserasprout.space>
2022-01-09 20:53:33 +0400

    typo

--- src/dipdup/codegen.py
+++ src/dipdup/codegen.py
@@ -293,7 +293,7 @@ class DipDupCodeGenerator:
                     '--disable-timestamp',
                 ]
                 self._logger.debug(' '.join(args))
-                subprocess.run(args, check=true)
+                subprocess.run(args, check=True)
 
     async def generate_handlers(self) -> None:
         """Generate handler stubs with typehints from templates if not exist"""
