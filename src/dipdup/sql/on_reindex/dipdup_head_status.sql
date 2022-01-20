aae03dc702531ef5cd13f28702b2b9b4d40238f8
Lev Gorodetskiy <github@droserasprout.space>
2022-01-20 14:08:41 +0400

    Fix view condition

--- src/dipdup/sql/on_reindex/dipdup_head_status.sql
+++ src/dipdup/sql/on_reindex/dipdup_head_status.sql
@@ -3,8 +3,8 @@ OR REPLACE VIEW dipdup_head_status AS
 SELECT
     name,
     CASE
-        WHEN timestamp - interval '2 minutes' < NOW() THEN 'OK'
-        ELSE 'OUTDATED'
+        WHEN timestamp < NOW() - interval '2 minutes' THEN 'OUTDATED'
+        ELSE 'OK'
     END AS status
 FROM
     dipdup_head;
