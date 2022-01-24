6e43ea59d7ec585ed7105cd36be5fd0fc6aa6809
Lev Gorodetskiy <github@droserasprout.space>
2022-01-24 13:13:04 +0400

    Increase dipdup_head_status timeout

--- src/dipdup/sql/on_reindex/dipdup_head_status.sql
+++ src/dipdup/sql/on_reindex/dipdup_head_status.sql
@@ -3,7 +3,7 @@ OR REPLACE VIEW dipdup_head_status AS
 SELECT
     name,
     CASE
-        WHEN timestamp < NOW() - interval '2 minutes' THEN 'OUTDATED'
+        WHEN timestamp < NOW() - interval '3 minutes' THEN 'OUTDATED'
         ELSE 'OK'
     END AS status
 FROM
