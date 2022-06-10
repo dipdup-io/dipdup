f401ceb7408dce79ade0a85fa72d4338b6c44ba0
Lev Gorodetskiy <github@droserasprout.space>
2022-06-10 13:40:28 +0400

    Do not skip integration tests in CI

--- tests/integration_tests/test_hasura.py
+++ tests/integration_tests/test_hasura.py
@@ -12,11 +12,9 @@ from dipdup.config import HasuraConfig
 from dipdup.config import PostgresDatabaseConfig
 from dipdup.dipdup import DipDup
 from dipdup.hasura import HasuraGateway
-from dipdup.utils import skip_ci
 from dipdup.utils.database import tortoise_wrapper
 
 
-@skip_ci
 class HasuraTest(IsolatedAsyncioTestCase):
     maxDiff = None
 
