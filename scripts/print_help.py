745f048d383bdf962cd4f5314b7735a68335347d
Lev Gorodetskiy <github@droserasprout.space>
2022-06-23 19:53:24 +0400

    Fix common-issues

--- scripts/print_help.py
+++ scripts/print_help.py
@@ -4,22 +4,22 @@ import dipdup.models as models
 
 example_exceptions = (
     exc.DipDupError(),
-    exc.DatasourceError('<error_message>', '<datasource_name>'),
-    exc.ConfigurationError('<error_message>'),
-    exc.DatabaseConfigurationError('<error_message>', models.Model),
+    exc.DatasourceError('[error_message]', '[datasource_name]'),
+    exc.ConfigurationError('[error_message]'),
+    exc.DatabaseConfigurationError('[error_message]', models.Model),
     exc.ReindexingRequiredError(
         exc.ReindexingReason.manual,
-        {'<context_key>': '<context_value>'},
+        {'[context_key]': '[context_value]'},
     ),
-    exc.InitializationRequiredError('<error_message>'),
-    exc.ProjectImportError('<module_qualname>', '<name>'),
-    exc.ContractAlreadyExistsError(MagicMock(), '<name>', 'tz1deadbeafdeadbeafdeadbeafdeadbeafdeadbeaf'),
-    exc.IndexAlreadyExistsError(MagicMock(), '<index_name>'),
-    exc.InvalidDataError(type, '<path>', {'<key>': '<value>'}),
-    exc.CallbackError('<callback_qualname>', Exception()),
-    exc.CallbackTypeError('<kind>', '<name>', '<arg>', type, type),
-    exc.HasuraError('<error_message>'),
-    exc.ConflictingHooksError('<old_hook>', '<new_hook>'),
+    exc.InitializationRequiredError('[error_message]'),
+    exc.ProjectImportError('[module_qualname]', '[name]'),
+    exc.ContractAlreadyExistsError(MagicMock(), '[name]', 'tz1deadbeafdeadbeafdeadbeafdeadbeafdeadbeaf'),
+    exc.IndexAlreadyExistsError(MagicMock(), '[index_name]'),
+    exc.InvalidDataError(type, '[path]', {'[key]': '[value]'}),
+    exc.CallbackError('[callback_qualname]', Exception()),
+    exc.CallbackTypeError('[kind]', '[name]', '[arg]', type, type),
+    exc.HasuraError('[error_message]'),
+    exc.ConflictingHooksError('[old_hook]', '[new_hook]'),
 )
 
 print('# Common issues')
