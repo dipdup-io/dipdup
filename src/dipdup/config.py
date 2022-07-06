499e5e07b9b1ec728166bf68f461ad0efabbca17
Lev Gorodetskiy <github@droserasprout.space>
2022-07-06 23:39:53 +0400

    Remove secrets from config `__repr__`

--- src/dipdup/config.py
+++ src/dipdup/config.py
@@ -126,7 +126,7 @@ class PostgresDatabaseConfig:
     database: str = DEFAULT_POSTGRES_DATABASE
     port: int = DEFAULT_POSTGRES_PORT
     schema_name: str = DEFAULT_POSTGRES_SCHEMA
-    password: str = ''
+    password: str = field(default='', repr=False)
     immune_tables: Tuple[str, ...] = field(default_factory=tuple)
     connection_timeout: int = 60
 
@@ -1023,7 +1023,7 @@ class HasuraConfig:
     """
 
     url: str
-    admin_secret: Optional[str] = None
+    admin_secret: Optional[str] = field(default=None, repr=False)
     create_source: bool = False
     source: str = 'default'
     select_limit: int = 100
