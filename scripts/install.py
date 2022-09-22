bda40c8294df08edb76fe4b2c4a3300916c7be69
Lev Gorodetskiy <github@droserasprout.space>
2022-09-21 21:52:00 -0300

    Split installer

--- scripts/install.py
+++ scripts/install.py
@@ -1,44 +1,48 @@
-#!python
-import os
 import subprocess
 import sys
-from functools import partial
-from shutil import rmtree
 from shutil import which
 from typing import NoReturn
 
 DEFAULT_REPO = 'https://github.com/dipdup-net/dipdup'
 DEFAULT_REF = 'master'
 
-run = partial(subprocess.run, check=True, shell=True)
+def run(*args, **kwargs):
+    return subprocess.run(*args, **kwargs, check=True, shell=True,)
 
 
-class bcolors:
-    OKBLUE = '\033[34m'
-    OKGREEN = '\033[92m'
-    FAIL = '\033[91m'
+class colors:
+    BLUE = '\033[34m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
     ENDC = '\033[0m'
 
 
-def echo(msg: str, color: str = bcolors.OKBLUE) -> None:
-    print(color + f'=> {msg}' + bcolors.ENDC)
+def echo(msg: str, color: str = colors.BLUE) -> None:
+    print(color + f'=> {msg}' + colors.ENDC)
 
 
 def fail(msg: str) -> NoReturn:
-    echo(msg, color=bcolors.FAIL)
+    echo(msg, color=colors.RED)
     sys.exit(1)
 
 
 def done(msg: str) -> NoReturn:
-    echo(msg, color=bcolors.OKGREEN)
+    echo(msg, color=colors.GREEN)
     sys.exit(0)
 
+def ask(msg: str, default: bool, quiet: bool) -> bool:
+    msg += ' [Y/n]' if default else ' [y/N]'
+    echo(msg, colors.YELLOW)
 
-def main(
-    quiet: bool = False,
-    repo: str = DEFAULT_REPO,
-    ref: str = DEFAULT_REF,
-) -> None:
+    if quiet:
+        return default
+    if default:
+        return input().lower() not in ('n', 'no')
+    else:
+        return input().lower() in ('y', 'yes')
+
+def main(quiet: bool) -> None:
     if sys.version_info < (3, 10):
         fail('DipDup requires Python 3.10')
 
@@ -49,55 +53,33 @@ def main(
         run('pip install --user -q pipx')
         run('python -m pipx ensurepath')
 
+    pipx_packages_raw = run('pipx list --short', capture_output=True).stdout
+    pipx_packages = {p.split()[0].decode() for p in pipx_packages_raw.splitlines()}
+
+    if 'dipdup' not in pipx_packages:
+        echo('Installing DipDup')
+        run('pipx install dipdup')
+    else:
+        echo('Updating DipDup')
+        run('pipx upgrade dipdup')
+
+    # NOTE: May be available system-wide
     if not which('cookiecutter'):
-        echo('Installing cookiecutter')
-        run('pipx install cookiecutter')
+        if ask('Install cookiecutter? Required for `dipdup new` command', True, quiet):
+            echo('Installing cookiecutter')
+            run('pipx install cookiecutter')
 
     if not which('poetry'):
-        echo('Installing poetry')
-        run('pipx install poetry')
+        if ask('Install poetry? Optional for `dipdup new` command', True, quiet):
+            echo('Installing poetry')
+            run('pipx install poetry')
 
-    cookiecutter_cmd = 'cookiecutter'
-    if repo.startswith(('git@', 'https://')):
-        echo('Using remote template')
-        cookiecutter_cmd += f' -f {repo} -c {ref} --directory cookiecutter'
-    else:
-        echo('Using local template')
-        cookiecutter_cmd += f' {repo}'
-    if quiet:
-        cookiecutter_cmd += ' --no-input'
-
-    rmtree(os.path.expanduser('~/.cookiecutters/dipdup'), ignore_errors=True)
-    run(cookiecutter_cmd)
-
-    for _dir in os.listdir(os.getcwd()):
-        if not os.path.isfile(f'{_dir}/dipdup.yml'):
-            continue
-        if os.path.isfile(f'{_dir}/poetry.lock'):
-            continue
-
-        echo(f'Found new project `{_dir}`')
-        run('git init', cwd=_dir)
-
-        echo('Running initial setup (can take a while)')
-        run('make install', cwd=_dir)
-
-        echo('Verifying project')
-        run('make lint', cwd=_dir)
-
-        done('Done! DipDup is ready to use.')
-
-    fail('No new projects found')
+    done('Done! DipDup is ready to use. Run `dipdup new` to create a new project')
 
 
 if __name__ == '__main__':
     args = sys.argv[1:]
-    quiet = False
-    if '-q' in args:
-        args.remove('-q')
-        quiet = True
+    if len(args) not in (0, 1):
+        fail('Usage: install.py [-q | --quiet]')
 
-    if len(args) not in (0, 1, 2):
-        fail('Usage: install.py [-q] [repo] [ref]')
-
-    main(quiet, *args)
+    main('--quiet' in args or '-q' in args)
--- src/dipdup/cli.py
+++ src/dipdup/cli.py
@@ -567,3 +567,40 @@ async def schema_export(ctx) -> None:
                 output += file.read() + '\n'
 
         echo(output)
+
+
+@cli.command()
+@click.pass_context
+@cli_wrapper
+async def new(ctx) -> None:
+    cookiecutter_cmd = 'cookiecutter'
+    if repo.startswith(('git@', 'https://')):
+        echo('Using remote template')
+        cookiecutter_cmd += f' -f {repo} -c {ref} --directory cookiecutter'
+    else:
+        echo('Using local template')
+        cookiecutter_cmd += f' {repo}'
+    if quiet:
+        cookiecutter_cmd += ' --no-input'
+
+    rmtree(os.path.expanduser('~/.cookiecutters/dipdup'), ignore_errors=True)
+    run(cookiecutter_cmd)
+
+    for _dir in os.listdir(os.getcwd()):
+        if not os.path.isfile(f'{_dir}/dipdup.yml'):
+            continue
+        if os.path.isfile(f'{_dir}/poetry.lock'):
+            continue
+
+        echo(f'Found new project `{_dir}`')
+        run('git init', cwd=_dir)
+
+        echo('Running initial setup (can take a while)')
+        run('make install', cwd=_dir)
+
+        echo('Verifying project')
+        run('make lint', cwd=_dir)
+
+        done('Done! DipDup is ready to use.')
+
+    fail('No new projects found')
