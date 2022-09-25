455dfd2589af7f9a1e5e1fca18f22af2cd9babdf
Lev Gorodetskiy <github@droserasprout.space>
2022-09-24 21:19:12 -0300

    Cleanup

--- src/dipdup/projects/base/tests/test_dipdup_indexer/test_dipdup_indexer.py
+++
@@ -1,8 +0,0 @@
-from unittest import IsolatedAsyncioTestCase
-
-import dipdup_indexer
-
-
-class ExampleTest(IsolatedAsyncioTestCase):
-    async def test_example(self):
-        assert dipdup_indexer
