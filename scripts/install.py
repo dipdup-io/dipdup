540481d722475ff76b3122f84b4df1f95cd10c44
Lev Gorodetskiy <github@droserasprout.space>
2022-09-26 15:09:23 -0300

    Rewrite installer again

--- scripts/install.py
+++ scripts/install.py
@@ -1,12 +1,20 @@
-#!/usr/bin/env python3
+"""This script (un)installs DipDup and its dependencies with pipx.
+
+WARNING: No imports allowed here except stdlib! Otherwise, `curl | python` magic will break.
+
+Some functions are importable for internal use in `dipdup.cli`.
+"""
 import os
 import subprocess
 import sys
 from shutil import which
 from typing import NoReturn
+import argparse
 
+GITHUB = 'https://github.com/dipdup-net/dipdup.git'
 
 def run(*args, **kwargs):
+    """Run shell command"""
     return subprocess.run(
         *args,
         **kwargs,
@@ -16,12 +24,22 @@ def run(*args, **kwargs):
 
 
 class colors:
+    """ANSI color codes"""
     BLUE = '\033[34m'
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
     RED = '\033[91m'
     ENDC = '\033[0m'
 
+import sys
+
+def get_base_prefix_compat():
+    """Get base/real prefix, or sys.prefix if there is none."""
+    return any((
+        getattr(sys, "base_prefix", None),
+        getattr(sys, "real_prefix", None),
+        sys.prefix,
+    ))
 
 def echo(msg: str, color: str = colors.BLUE) -> None:
     print(color + f'=> {msg}' + colors.ENDC)
@@ -48,48 +66,76 @@ def ask(msg: str, default: bool, quiet: bool) -> bool:
     else:
         return input().lower() in ('y', 'yes')
 
+def ensure_git() -> None:
+    """Ensure git is installed"""
+    if which('git'):
+        return
+
+    fail('git is required to install DipDup from ref')
 
 def ensure_pipx() -> None:
-    if not which('pipx'):
-        echo('Installing pipx')
-        run('pip install --user -q pipx')
-        run('python -m pipx ensurepath')
+    """Ensure pipx is installed for current user"""
+    if which('pipx'):
+        return
 
+    echo('Installing pipx')
+    run('pip install --user -q pipx')
+    run('python -m pipx ensurepath')
+
+def ensure_poetry(quiet: bool) -> None:
+    """Ensure poetry is installed for current user"""
+    if which('poetry'):
+        return
+
+    if ask('Install poetry? Optional for `dipdup new` command', True, quiet):
+        echo('Installing poetry')
+        run('pipx install poetry')
 
 def get_pipx_packages() -> set[str]:
+    """Get installed pipx packages"""
     ensure_pipx()
     pipx_packages_raw = run('pipx list --short', capture_output=True).stdout
     return {p.split()[0].decode() for p in pipx_packages_raw.splitlines()}
 
 
-def install(quiet: bool, local: bool) -> None:
+def install(
+    quiet: bool,
+    force: bool,
+    ref: str | None,
+    path: str | None,
+) -> None:
+    """Install DipDup and its dependencies with pipx"""
+    if ref and path:
+        fail('Specify either ref or path, not both')
+
+    force_str = '--force' if force else ''
     pipx_packages = get_pipx_packages()
 
     if 'dipdup' not in pipx_packages:
-        if local:
-            echo(f'Installing DipDup from `{os.getcwd()}`')
-            run('pipx install .')
+        if path:
+            echo(f'Installing DipDup from `{path}`')
+            run(f'pipx install {path} {force_str}')
+        elif ref:
+            echo(f'Installing DipDup from `{ref}`')
+            run(f'pipx install git+{GITHUB}@{ref} {force_str}')
         else:
             echo('Installing DipDup from PyPI')
-            run('pipx install dipdup')
+            run(f'pipx install dipdup {force_str}')
     else:
         echo('Updating DipDup')
-        run('pipx upgrade dipdup')
+        run(f'pipx upgrade dipdup {force_str}')
 
     if 'datamodel-code-generator' not in pipx_packages:
-        run('pipx install datamodel-code-generator')
+        run(f'pipx install datamodel-code-generator {force_str}')
     else:
-        run('pipx upgrade datamodel-code-generator')
+        run(f'pipx upgrade datamodel-code-generator {force_str}')
 
-    if not which('poetry'):
-        if ask('Install poetry? Optional for `dipdup new` command', True, quiet):
-            echo('Installing poetry')
-            run('pipx install poetry')
+    ensure_poetry(quiet)
 
     done('Done! DipDup is ready to use.\nRun `dipdup new` to create a new project or `dipdup` to see all available commands.')
 
-
-def uninstall() -> None:
+def uninstall(quiet: bool) -> NoReturn:
+    """Uninstall DipDup and its dependencies with pipx"""
     pipx_packages = get_pipx_packages()
 
     if 'dipdup' in pipx_packages:
@@ -97,25 +143,43 @@ def uninstall() -> None:
         run('pipx uninstall dipdup')
 
     if 'datamodel-code-generator' in pipx_packages:
-        echo('Uninstalling datamodel-code-generator')
-        run('pipx uninstall datamodel-code-generator')
+        if ask('Uninstall datamodel-code-generator?', True, quiet):
+            echo('Uninstalling datamodel-code-generator')
+            run('pipx uninstall datamodel-code-generator')
 
     done('Done! DipDup is uninstalled.')
 
-
-if __name__ == '__main__':
-    if sys.version_info < (3, 10):
+def _check_system() -> None:
+    if not sys.version.startswith('3.10'):
         fail('DipDup requires Python 3.10')
 
-    args = sys.argv[1:]
-    if len(args) not in (0, 1, 2):
-        fail('usage: install.py [-q | --quiet] | [-l | --local] | [-u | --uninstall')
+    # NOTE: Show warning if user is root
+    if os.geteuid() == 0:
+        echo('WARNING: Running as root, this is not generally recommended', colors.YELLOW)        
 
+    # NOTE: Show warning if user is in virtualenv
+    if sys.base_prefix != sys.prefix:
+        echo('WARNING: Running in virtualenv, this script affects only current user', colors.YELLOW)
+
+if __name__ == '__main__':
     echo('Welcome to DipDup installer')
 
-    if '-u' in args or '--uninstall' in args:
-        uninstall()
+    _check_system()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-q', '--quiet', action='store_true', help='Use default answers for all questions')
+    parser.add_argument('-f', '--force', action='store_true', help='Force reinstall')
+    parser.add_argument('-r', '--ref', help='Install DipDup from a specific git ref')
+    parser.add_argument('-p', '--path', help='Install DipDup from a local path')
+    parser.add_argument('-u', '--uninstall', action='store_true', help='Uninstall DipDup')
+    args = parser.parse_args()
+
+    if args.uninstall:
+        uninstall(args.quiet)
     else:
-        quiet = '--quiet' in args or '-q' in args
-        local = '--local' in args or '-l' in args
-        install(quiet, local)
+        install(
+            quiet=args.quiet,
+            force=args.force,
+            ref=args.ref.strip() if args.ref else None,
+            path=args.path.strip() if args.path else None,
+        )
