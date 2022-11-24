da0abb6b7b46b2f8470cbef2e2d8f341d01cea25
Lev Gorodetskiy <github@droserasprout.space>
2022-11-24 13:50:57 -0300

    Refactor tests

--- tests/test_datasources/test_ipfs.py
+++ tests/test_datasources/test_ipfs.py
@@ -6,17 +6,16 @@ from dipdup.config import HTTPConfig
 from dipdup.datasources.ipfs.datasource import IpfsDatasource
 
 
-class IpfsDatasourceTest(IsolatedAsyncioTestCase):
-    async def test_ipfs_datasource(self) -> None:
-        ipfs = IpfsDatasource(
-            url=DEFAULT_IPFS_URL,
-            http_config=HTTPConfig(
-                replay_path=str(Path(__file__).parent.parent / 'replays'),
-            ),
-        )
-        async with ipfs:
-            file = await ipfs.get('bafybeifx7yeb55armcsxwwitkymga5xf53dxiarykms3ygqic223w5sk3m')
-            self.assertEqual(file[:5].decode(), 'Hello')
+async def test_ipfs_datasource() -> None:
+    ipfs = IpfsDatasource(
+        url=DEFAULT_IPFS_URL,
+        http_config=HTTPConfig(
+            replay_path=str(Path(__file__).parent.parent / 'replays'),
+        ),
+    )
+    async with ipfs:
+        file = await ipfs.get('bafybeifx7yeb55armcsxwwitkymga5xf53dxiarykms3ygqic223w5sk3m')
+        assert file[:5].decode() == 'Hello'
 
-            file = await ipfs.get('QmSgSC7geYH3Ae4SpUHy4KutxqNH9ESKBGXoCN4JQdbtEz/package.json')
-            self.assertEqual(file['name'], 'json-buffer')
+        file = await ipfs.get('QmSgSC7geYH3Ae4SpUHy4KutxqNH9ESKBGXoCN4JQdbtEz/package.json')
+        file['name'] == 'json-buffer'
--- tests/test_datasources/test_metadata.py
+++ tests/test_datasources/test_metadata.py
@@ -1,20 +1,18 @@
-from unittest import IsolatedAsyncioTestCase
 
 import aiohttp
 
 from dipdup.datasources.metadata.enums import MetadataNetwork
 
 
-class MetadataDatasourceTest(IsolatedAsyncioTestCase):
-    async def test_metadata_networks(self) -> None:
-        async with aiohttp.ClientSession() as session:
-            async with session.post(
-                'https://metadata.dipdup.net/v1/graphql',
-                json={
-                    'query': 'query MetadataNetworks { contract_metadata(distinct_on: network) { network } }',
-                    'operationName': 'MetadataNetworks',
-                },
-            ) as response:
-                res = await response.json()
-                for network in res['data']['contract_metadata']:
-                    self.assertIn(network['network'], MetadataNetwork.__members__)
+async def test_metadata_networks() -> None:
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            'https://metadata.dipdup.net/v1/graphql',
+            json={
+                'query': 'query MetadataNetworks { contract_metadata(distinct_on: network) { network } }',
+                'operationName': 'MetadataNetworks',
+            },
+        ) as response:
+            res = await response.json()
+            for network in res['data']['contract_metadata']:
+                assert network['network'] in MetadataNetwork.__members__
