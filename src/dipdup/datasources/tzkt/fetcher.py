dc3a279b8bb350832aa0b7a1abf650d0e0c6e738
Lev Gorodetskiy <github@droserasprout.space>
2022-11-28 18:12:02 -0300

    Rewrite fetchers (green but slow)

--- src/dipdup/datasources/tzkt/fetcher.py
+++ src/dipdup/datasources/tzkt/fetcher.py
@@ -1,10 +1,12 @@
+from __future__ import annotations
+
 import logging
 from abc import ABC
 from abc import abstractmethod
 from collections import defaultdict
 from collections import deque
-from enum import Enum
 from typing import TYPE_CHECKING
+from typing import Any
 from typing import AsyncGenerator
 from typing import AsyncIterator
 from typing import Generic
@@ -20,43 +22,19 @@ if TYPE_CHECKING:
     from dipdup.datasources.tzkt.datasource import TzktDatasource
 
 
-class FetcherEndpoint(Enum):
-    """Represents multiple TzKT calls to be merged into a single batch of operations"""
-
-    sender_transactions = 'sender_transactions'
-    target_transactions = 'target_transactions'
-    originations = 'originations'
-
-
-class FetcherFilter(Enum):
-    address = 'address'
-    hash = 'hash'
-
-
-FetcherChannel = tuple[FetcherEndpoint, FetcherFilter]
-
-
-operation_fetcher_channels = (
-    (FetcherEndpoint.sender_transactions, FetcherFilter.address),
-    (FetcherEndpoint.target_transactions, FetcherFilter.address),
-    (FetcherEndpoint.originations, FetcherFilter.address),
-    (FetcherEndpoint.sender_transactions, FetcherFilter.hash),
-    (FetcherEndpoint.target_transactions, FetcherFilter.hash),
-    (FetcherEndpoint.originations, FetcherFilter.hash),
-)
-
-
 class HasLevel(Protocol):
     level: int
 
 
-HasLevelT = TypeVar('HasLevelT', bound=HasLevel)
+Level = int
+FetcherBufferT = TypeVar('FetcherBufferT', bound=HasLevel)
+FetcherFilterT = TypeVar('FetcherFilterT')
 
 
 async def yield_by_level(
-    iterable: AsyncIterator[tuple[HasLevelT, ...]]
-) -> AsyncGenerator[tuple[int, tuple[HasLevelT, ...]], None]:
-    items: tuple[HasLevelT, ...] = ()
+    iterable: AsyncIterator[tuple[FetcherBufferT, ...]]
+) -> AsyncGenerator[tuple[Level, tuple[FetcherBufferT, ...]], None]:
+    items: tuple[FetcherBufferT, ...] = ()
 
     async for item_batch in iterable:
         items = items + item_batch
@@ -78,9 +56,145 @@ async def yield_by_level(
         yield items[0].level, items
 
 
-class DataFetcher(Generic[HasLevelT], ABC):
+def dedup_operations(operations: tuple[OperationData, ...]) -> tuple[OperationData, ...]:
+    """Merge and sort operations fetched from multiple endpoints"""
+    return tuple(
+        sorted(
+            (({op.id: op for op in operations}).values()),
+            key=lambda op: op.id,
+        )
+    )
+
+
+def get_operations_head(operations: tuple[OperationData, ...]) -> int:
+    """Get latest block level (head) of sorted operations batch"""
+    for i in range(len(operations) - 1)[::-1]:
+        if operations[i].level != operations[i + 1].level:
+            return operations[i].level
+    return operations[0].level
+
+
+class FetcherChannel(ABC, Generic[FetcherBufferT, FetcherFilterT]):
+    def __init__(
+        self,
+        buffer: defaultdict[Level, deque[FetcherBufferT]],
+        filter: set[FetcherFilterT],
+        first_level: int,
+        last_level: int,
+        datasource: 'TzktDatasource',
+    ) -> None:
+        super().__init__()
+        self._buffer = buffer
+        self._filter = filter
+        self._first_level = first_level
+        self._last_level = last_level
+        self._datasource = datasource
+
+        self._head: int = 0
+        self._offset: int = 0
+
+    @property
+    def head(self) -> int:
+        return self._head
+
+    @property
+    def fetched(self) -> bool:
+        return self._head >= self._last_level
+
     @abstractmethod
-    def fetch_by_level(self) -> AsyncIterator[tuple[int, tuple[HasLevelT, ...]]]:
+    async def fetch(self) -> None:
+        """Fetch a single `requets_limit` batch of items, bump channel offset"""
+        ...
+
+
+class OriginationAddressFetcherChannel(FetcherChannel[OperationData, str]):
+    async def fetch(self) -> None:
+        # FIXME: No pagination because of URL length limit workaround
+        originations = await self._datasource.get_originations(
+            addresses=self._filter,
+            first_level=self._first_level,
+            last_level=self._last_level,
+        )
+
+        for op in originations:
+            self._buffer[op.level].append(op)
+
+        self._head = self._last_level
+        self._offset = self._last_level
+
+
+class OriginationHashFetcherChannel(FetcherChannel[OperationData, int]):
+    # FIXME: Not implemented
+    async def fetch(self) -> None:
+        self._head = self._last_level
+
+
+class TransactionAddressFetcherChannel(FetcherChannel[OperationData, str]):
+    def __init__(
+        self,
+        buffer: defaultdict[Level, deque[OperationData]],
+        filter: set[str],
+        first_level: int,
+        last_level: int,
+        datasource: 'TzktDatasource',
+        field: str,
+    ) -> None:
+        super().__init__(buffer, filter, first_level, last_level, datasource)
+        self._field = field
+
+    async def fetch(self) -> None:
+        transactions = await self._datasource.get_transactions(
+            field=self._field,
+            addresses=self._filter,
+            offset=self._offset,
+            first_level=self._first_level,
+            last_level=self._last_level,
+        )
+
+        for op in transactions:
+            level = op.level
+            self._buffer[level].append(op)
+
+        if len(transactions) < self._datasource.request_limit:
+            self._head = self._last_level
+        else:
+            self._offset = transactions[-1].id
+            self._head = get_operations_head(transactions)
+
+
+class TransactionHashFetcherChannel(FetcherChannel[OperationData, int]):
+    def __init__(
+        self,
+        buffer: defaultdict[Level, deque[OperationData]],
+        filter: set[int],
+        first_level: int,
+        last_level: int,
+        datasource: 'TzktDatasource',
+        field: str,
+    ) -> None:
+        super().__init__(buffer, filter, first_level, last_level, datasource)
+        self._field = field
+
+    # FIXME: Not implemented
+    async def fetch(self) -> None:
+        self._head = self._last_level
+
+
+class DataFetcher(ABC, Generic[FetcherBufferT]):
+    def __init__(
+        self,
+        datasource: 'TzktDatasource',
+        first_level: int,
+        last_level: int,
+    ) -> None:
+        self._datasource = datasource
+        self._first_level = first_level
+        self._last_level = last_level
+        self._buffer: defaultdict[Level, deque[FetcherBufferT]] = defaultdict(deque)
+        self._head = 0
+
+    @abstractmethod
+    def fetch_by_level(self) -> AsyncIterator[tuple[int, tuple[FetcherBufferT, ...]]]:
         ...
 
 
@@ -101,132 +215,78 @@ class OperationFetcher(DataFetcher[OperationData]):
         origination_hashes: set[int],
         migration_originations: tuple[OperationData, ...] = (),
     ) -> None:
-        self._datasource = datasource
-        self._first_level = first_level
-        self._last_level = last_level
+        super().__init__(datasource, first_level, last_level)
         self._transaction_addresses = transaction_addresses
         self._transaction_hashes = transaction_hashes
         self._origination_addresses = origination_addresses
         self._origination_hashes = origination_hashes
 
         self._logger = logging.getLogger('dipdup.tzkt')
-        self._head: int = 0
-        self._heads: dict[FetcherChannel, int] = {}
-        self._offsets: dict[FetcherChannel, int] = {}
-        self._fetched: dict[FetcherChannel, bool] = {}
 
-        self._operations: defaultdict[int, deque[OperationData]] = defaultdict(deque)
+        # FIXME: Why migrations are prefetched?
         for origination in migration_originations:
-            self._operations[origination.level].append(origination)
-
-    def _get_operations_head(self, operations: tuple[OperationData, ...]) -> int:
-        """Get latest block level (head) of sorted operations batch"""
-        for i in range(len(operations) - 1)[::-1]:
-            if operations[i].level != operations[i + 1].level:
-                return operations[i].level
-        return operations[0].level
-
-    async def _fetch_originations(self) -> None:
-        """Fetch a single batch of originations, bump channel offset"""
-        key = FetcherEndpoint.originations, FetcherFilter.address
-        if not self._origination_addresses:
-            self._fetched[key] = True
-            self._heads[key] = self._last_level
-        if self._fetched[key]:
-            return
-
-        self._logger.debug('Fetching originations of %s', self._origination_addresses)
-
-        # FIXME: No pagination because of URL length limit workaround
-        originations = await self._datasource.get_originations(
-            addresses=self._origination_addresses,
-            first_level=self._first_level,
-            last_level=self._last_level,
-        )
-
-        for op in originations:
-            level = op.level
-            self._operations[level].append(op)
-
-        self._logger.debug('Got %s', len(originations))
-        self._fetched[key] = True
-        self._heads[key] = self._last_level
-
-    async def _fetch_transactions(self, field: str) -> None:
-        """Fetch a single batch of transactions, bump channel offset"""
-        key = getattr(FetcherEndpoint, field + '_transactions')
-        if not self._transaction_addresses:
-            self._fetched[key] = True
-            self._heads[key] = self._last_level
-        if self._fetched[key]:
-            return
-
-        self._logger.debug('Fetching %s transactions of %s', field, self._transaction_addresses)
-
-        transactions = await self._datasource.get_transactions(
-            field=field,
-            addresses=self._transaction_addresses,
-            offset=self._offsets[key],
-            first_level=self._first_level,
-            last_level=self._last_level,
-        )
-
-        for op in transactions:
-            level = op.level
-            self._operations[level].append(op)
-
-        self._logger.debug('Got %s', len(transactions))
-
-        if len(transactions) < self._datasource.request_limit:
-            self._fetched[key] = True
-            self._heads[key] = self._last_level
-        else:
-            self._offsets[key] = transactions[-1].id
-            self._heads[key] = self._get_operations_head(transactions)
-
-    @staticmethod
-    def dedup_operations(operations: tuple[OperationData, ...]) -> tuple[OperationData, ...]:
-        """Merge and sort operations fetched from multiple endpoints"""
-        return tuple(
-            sorted(
-                (({op.id: op for op in operations}).values()),
-                key=lambda op: op.id,
-            )
-        )
+            self._buffer[origination.level].append(origination)
 
     async def fetch_by_level(self) -> AsyncIterator[tuple[int, tuple[OperationData, ...]]]:
         """Iterate over operations fetched with multiple REST requests with different filters.
 
         Resulting data is splitted by level, deduped, sorted and ready to be processed by OperationIndex.
         """
-        for channel in operation_fetcher_channels:
-            self._heads[channel] = 0
-            self._offsets[channel] = 0
-            self._fetched[channel] = False
+        channel_kwargs = {
+            'buffer': self._buffer,
+            'datasource': self._datasource,
+            'first_level': self._first_level,
+            'last_level': self._last_level,
+        }
+        channels: tuple[FetcherChannel[OperationData, Any], ...] = (
+            TransactionAddressFetcherChannel(
+                filter=self._transaction_addresses,
+                field='sender',
+                **channel_kwargs,  # type: ignore[arg-type]
+            ),
+            TransactionAddressFetcherChannel(
+                filter=self._transaction_addresses,
+                field='target',
+                **channel_kwargs,  # type: ignore[arg-type]
+            ),
+            TransactionHashFetcherChannel(
+                filter=self._transaction_hashes,
+                field='sender',
+                **channel_kwargs,  # type: ignore[arg-type]
+            ),
+            TransactionHashFetcherChannel(
+                filter=self._transaction_hashes,
+                field='target',
+                **channel_kwargs,  # type: ignore[arg-type]
+            ),
+            OriginationAddressFetcherChannel(
+                filter=self._origination_addresses,
+                **channel_kwargs,  # type: ignore[arg-type]
+            ),
+            OriginationHashFetcherChannel(
+                filter=self._origination_hashes,
+                **channel_kwargs,  # type: ignore[arg-type]
+            ),
+        )
 
         while True:
-            min_head = sorted(self._heads.items(), key=lambda x: x[1])[0][0]
+            min_channel = sorted(channels, key=lambda x: x.head)[0]
+            await min_channel.fetch()
 
-            if min_head == (FetcherEndpoint.originations, FetcherFilter.address):
-                await self._fetch_originations()
-            elif min_head == (FetcherEndpoint.target_transactions, FetcherFilter.address):
-                await self._fetch_transactions('target')
-            elif min_head == (FetcherEndpoint.sender_transactions, FetcherFilter.address):
-                await self._fetch_transactions('sender')
-            else:
-                raise NotImplementedError
+            # NOTE: It's a different channel now, but with greater head level
+            min_channel = sorted(channels, key=lambda x: x.head)[0]
+            min_head = min_channel.head
 
-            head = min(self._heads.values())
-            while self._head <= head:
-                if self._head in self._operations:
-                    operations = self._operations.pop(self._head)
-                    yield self._head, self.dedup_operations(tuple(operations))
+            while self._head <= min_head:
+                if self._head in self._buffer:
+                    operations = self._buffer.pop(self._head)
+                    yield self._head, dedup_operations(tuple(operations))
                 self._head += 1
 
-            if all(self._fetched.values()):
+            if all(c.fetched for c in channels):
                 break
 
-        if self._operations:
+        if self._buffer:
             raise RuntimeError('Operations left in queue')
 
 
@@ -241,10 +301,8 @@ class BigMapFetcher(DataFetcher[BigMapData]):
         big_map_addresses: set[str],
         big_map_paths: set[str],
     ) -> None:
+        super().__init__(datasource, first_level, last_level)
         self._logger = logging.getLogger('dipdup.tzkt')
-        self._datasource = datasource
-        self._first_level = first_level
-        self._last_level = last_level
         self._big_map_addresses = big_map_addresses
         self._big_map_paths = big_map_paths
 
@@ -274,14 +332,12 @@ class TokenTransferFetcher(DataFetcher[TokenTransferData]):
         first_level: int,
         last_level: int,
     ) -> None:
+        super().__init__(datasource, first_level, last_level)
         self._logger = logging.getLogger('dipdup.tzkt')
-        self._datasource = datasource
         self._token_addresses = token_addresses
         self._token_ids = token_ids
         self._from_addresses = from_addresses
         self._to_addresses = to_addresses
-        self._first_level = first_level
-        self._last_level = last_level
 
     async def fetch_by_level(self) -> AsyncIterator[tuple[int, tuple[TokenTransferData, ...]]]:
         token_transfer_iter = self._datasource.iter_token_transfers(
