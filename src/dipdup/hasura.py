eaa901dbcc10e3943e191f029b89965ac5654062
Igor Sereda <sereda.igor.s@gmail.com>
2023-01-14 14:21:48 +0700

    Arbitrary hasura metadata customisations

--- src/dipdup/hasura.py
+++ src/dipdup/hasura.py
@@ -189,6 +189,8 @@ class HasuraGateway(HTTPGateway):
 
         await self._replace_metadata(metadata)
 
+        await self._apply_custom_metadata_requests()
+
         # TODO: Find out why it is necessary
         # NOTE: Fetch metadata once again and save its hash for future comparisons
         metadata = await self._fetch_metadata()
@@ -642,3 +644,21 @@ class HasuraGateway(HTTPGateway):
         if source_field := field.source_field:
             return field.model._meta.fields_db_projection[source_field]
         return field.model_field_name + '_id'
+
+    def _iterate_hasura_metadata_requests(self) -> Iterator:
+        package = importlib.import_module(self._package)
+        hasura_path = Path(package.__path__[0]) / 'hasura'
+        for file in iter_files(hasura_path, '.json'):
+            yield file
+
+    async def _apply_custom_metadata_requests(self):
+        for file in self._iterate_hasura_metadata_requests():
+            try:
+                self._logger.info('Executing custom metadata request `%s`', file.name)
+                payload = json.loads(file.read())
+                await self._hasura_request(
+                    endpoint='metadata',
+                    json=payload,
+                )
+            except json.JSONDecodeError:
+                self._logger.error('Invalid JSON in `%s`, skipped', file.name)
