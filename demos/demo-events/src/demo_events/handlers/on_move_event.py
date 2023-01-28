d8fd819947b9d7795452025d731dd9332d744400
Lev Gorodetskiy <github@droserasprout.space>
2023-01-27 22:27:23 -0300

    Add missing config docstrings, bump deps and testnet, update demo_events (#600)

---
+++ demos/demo-events/src/demo_events/handlers/on_move_event.py
@@ -0,0 +1,11 @@
+from demo_events import models as models
+from demo_events.types.events_contract.event.move import MovePayload
+from dipdup.context import HandlerContext
+from dipdup.models import Event
+
+
+async def on_move_event(
+    ctx: HandlerContext,
+    event: Event[MovePayload],
+) -> None:
+    ...
---
+++ demos/demo-events/src/demo_events/handlers/on_roll_event.py
@@ -0,0 +1,11 @@
+from demo_events import models as models
+from demo_events.types.events_contract.event.roll import RollPayload
+from dipdup.context import HandlerContext
+from dipdup.models import Event
+
+
+async def on_roll_event(
+    ctx: HandlerContext,
+    event: Event[RollPayload],
+) -> None:
+    ...
