57cc098a6e9869a9c33b72d22d991071ee77d122
droserasprout <droserasprout@users.noreply.github.com>
2023-01-28 16:31:46 +0000

    deploy: 105089c55c69c6548632e16ec0b93c29e39387ef

--- docs/conf.py
+++ docs/conf.py
@@ -53,3 +53,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# NOTE: Nice dataclasses
+autodoc_typehints = 'description'
+autodoc_typehints_format = 'short'
