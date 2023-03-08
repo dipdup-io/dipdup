1d6a0c6a8498e463837e1d040f4dda0655e54436
droserasprout <droserasprout@users.noreply.github.com>
2023-03-08 17:29:41 +0000

    deploy: 7968e5ebf98c27cd720bd60b33da13b773d28ce7

--- update_cookiecutter.py
+++ update_cookiecutter.py
@@ -4,6 +4,6 @@ from pathlib import Path
 from dipdup.project import BaseProject
 
 path = Path(__file__).parent.parent / 'docs' / 'cookiecutter.json'
-project = BaseProject()
+project = BaseProject()  # type: ignore[call-arg]
 project.run(quiet=True, replay=None)
 project.write_cookiecutter_json(path)
