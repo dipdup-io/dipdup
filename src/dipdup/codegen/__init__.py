08fe639d1a560ecfb114d603bcb5c74b0c11682d
Lev Gorodetskiy <github@droserasprout.space>
2023-03-31 22:05:51 +0400

    Drop pytezos and slim images (#623)

--- src/dipdup/codegen/__init__.py
+++ src/dipdup/codegen/__init__.py
@@ -14,7 +14,7 @@ from pydantic import BaseModel
 from dipdup.config import CallbackMixin
 from dipdup.config import DipDupConfig
 from dipdup.datasources import Datasource
-from dipdup.exceptions import FeatureAvailabilityError
+from dipdup.exceptions import FrameworkException
 from dipdup.package import KEEP_MARKER
 from dipdup.package import PYTHON_MARKER
 from dipdup.package import DipDupPackage
@@ -117,10 +117,7 @@ class CodeGenerator(ABC):
 
         datamodel_codegen = which('datamodel-codegen')
         if not datamodel_codegen:
-            raise FeatureAvailabilityError(
-                feature='codegen',
-                reason='datamodel-codegen is not installed. Are you in the `-slim` Docker image? If not - run `dipdup-install`.',
-            )
+            raise FrameworkException('datamodel-codegen is not installed')
 
         class_name = self.get_typeclass_name(schema_path)
 
