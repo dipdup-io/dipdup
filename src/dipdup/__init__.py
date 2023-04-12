a684712995625dc8514284f02bfa6b4901e71d97
Wizard1209 <34334729+Wizard1209@users.noreply.github.com>
2023-04-12 19:58:03 +0600

    (2/2) Introduce spec_version 2.0 (#654)
    Co-authored-by: Vladimir Bobrikov <vladimir_bobrikov@pm.me>
    Co-authored-by: Lev Gorodetskiy <github@droserasprout.space>

--- src/dipdup/__init__.py
+++ src/dipdup/__init__.py
@@ -7,20 +7,3 @@ import importlib.metadata
 # NOTE: Load version from package
 __version__ = importlib.metadata.version('dipdup')
 __spec_version__ = '1.2'
-
-version = tuple(__version__.split('.'))
-major_version = version[0]
-minor_version = f'{version[0]}.{version[1]}'
-
-spec_version_mapping = {
-    '0.1': '<=0.4.3',
-    '1.0': '>=1.0.0, <=1.1.2',
-    '1.1': '>=2.0.0, <=2.0.9',
-    '1.2': '>=3.0.0',
-}
-spec_reindex_mapping = {
-    '0.1': False,
-    '1.0': False,
-    '1.1': True,
-    '1.2': True,
-}
