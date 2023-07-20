8777d323a050b0bae31bfcf1cba6ca870cf65092
Lev Gorodetskiy <github@droserasprout.space>
2023-07-20 15:02:40 -0300

    Rename references

--- docs/build-sphinx.sh
+++ docs/build-sphinx.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cli_reference=7.reference/1.cli-reference.md
-config_reference=7.reference/2.reference.md
-context_reference=7.reference/3.context-reference.md
-cli_reference_title=cli
-config_reference_title=config
-context_reference_title=context
+cli_reference=7.references/1.cli.md
+config_reference=7.references/2.config.md
+context_reference=7.references/3.context.md
+cli_reference_title="CLI"
+config_reference_title="Config"
+context_reference_title="Context (ctx)"
 
 sphinx-build -M html . _build
 
-python dump_references.py _build/html/cli-reference.html $cli_reference $cli_reference_title
-python dump_references.py _build/html/config-reference.html $config_reference $config_reference_title
-python dump_references.py _build/html/context-reference.html $context_reference $context_reference_title
+python dump_references.py _build/html/cli-reference.html $cli_reference "$cli_reference_title"
+python dump_references.py _build/html/config-reference.html $config_reference "$config_reference_title"
+python dump_references.py _build/html/context-reference.html $context_reference "$context_reference_title"
