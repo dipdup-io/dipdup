71769811d30fc1fddb437acef9aabf89f729cd29
droserasprout <droserasprout@users.noreply.github.com>
2023-07-30 13:28:34 +0000

    deploy: c5b464280f988682a8603814e6931b2c1035ffb5

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 """This script (un)installs DipDup and its dependencies with pipx.
 
 WARNING: No imports allowed here except stdlib! Otherwise, `curl | python` magic will break.
-And no 3.11-only code too. Just to print nice colored "not supported" message instead of crashing.
+And no pre-3.10 code too. Just to print nice colored "not supported" message instead of crashing.
 
 Some functions are importable to use in `dipdup.cli`.
 This script is also available as `dipdup-install` or `python -m dipdup.install`.
@@ -11,39 +11,24 @@ import os
 import subprocess
 import sys
 from pathlib import Path
+from shutil import rmtree
 from shutil import which
 from typing import Any
 from typing import Dict
 from typing import NoReturn
 from typing import Optional
 from typing import Set
-from typing import cast
 
 GITHUB = 'https://github.com/dipdup-io/dipdup.git'
 WHICH_CMDS = (
-    'bash',
-    'python3.11',
+    'python3',
     'pipx',
     'dipdup',
     'datamodel-codegen',
-    'pdm',
+    'poetry',
     'pyvenv',
     'pyenv',
 )
-
-WELCOME_ASCII = """\0
-        ____   _         ____              
-       / __ \ (_)____   / __ \ __  __ ____ 
-      / / / // // __ \ / / / // / / // __ \\
-     / /_/ // // /_/ // /_/ // /_/ // /_/ /
-    /_____//_// .___//_____/ \__,_// .___/ 
-             /_/                  /_/      
-"""
-EPILOG = """\0
-Documentation:         https://dipdup.io/docs
-GitHub:                https://github.com/dipdup-io/dipdup
-Discord:               https://discord.gg/NbANhqCJHA
-"""
 
 
 class Colors:
@@ -70,46 +55,60 @@ def done(msg: str) -> NoReturn:
     sys.exit(0)
 
 
+def ask(msg: str, default: bool, quiet: bool) -> bool:
+    msg += ' [Y/n]' if default else ' [y/N]'
+    echo(msg, Colors.YELLOW)
+
+    if quiet:
+        return default
+    if default:
+        return input().lower() not in ('n', 'no')
+    else:
+        return input().lower() in ('y', 'yes')
+
+
 # NOTE: DipDup has `tabulate` dep, don't use this one elsewhere
 def _tab(text: str, indent: int = 20) -> str:
     return text + ' ' * (indent - len(text))
 
 
 class DipDupEnvironment:
-    def __init__(self) -> None:
+    def __init__(self, quiet: bool = False) -> None:
         self._os = os.uname().sysname
         self._arch = os.uname().machine
+        self._quiet = quiet
         self._commands: Dict[str, Optional[str]] = {}
         self._pipx_packages: Set[str] = set()
 
     def refresh(self) -> None:
+        if not self._quiet and not self._commands:
+            print()
+            print(_tab('OS:') + self._os)
+            print(_tab('Arch:') + self._arch)
+            print(_tab('Python:') + sys.version)
+            print(_tab('PATH:') + os.environ['PATH'])
+            print()
+
         for command in WHICH_CMDS:
             old, new = self._commands.get(command), which(command)
             if old == new:
                 continue
             self._commands[command] = new
+            self._quiet or print(_tab(command) + (new or ''))
 
-    def print(self) -> None:
         print()
-        print(WELCOME_ASCII)
-        print(EPILOG)
-        print()
-        print(_tab('OS:') + f'{self._os} ({self._arch})')
-        print(_tab('Python:') + sys.version)
-        print(_tab('PATH:') + os.environ['PATH'])
-        print(_tab('PYTHONPATH:') + os.environ.get('PYTHONPATH', ''))
-        print()
-        for command, path in self._commands.items():
-            print(_tab(f'{command}:') + (path or ''))
-        print(_tab('pipx packages:') + ', '.join(self._pipx_packages) + '\n')
 
     def refresh_pipx(self) -> None:
         """Get installed pipx packages"""
         self.ensure_pipx()
         pipx_packages_raw = self.run_cmd('pipx', 'list', '--short', capture_output=True).stdout
         self._pipx_packages = {p.split()[0].decode() for p in pipx_packages_raw.splitlines()}
+        self._quiet or print(_tab('pipx packages:') + ', '.join(self._pipx_packages) + '\n')
 
-    def prepare(self) -> None:
+    def check(self) -> None:
+        if not sys.version.startswith('3.10'):
+            fail('DipDup requires Python 3.10')
+
         # NOTE: Show warning if user is root
         if os.geteuid() == 0:
             echo('WARNING: Running as root, this is not generally recommended', Colors.YELLOW)
@@ -120,6 +119,9 @@ class DipDupEnvironment:
 
         self.refresh()
         self.refresh_pipx()
+
+        if self._commands.get('pyenv'):
+            echo('WARNING: pyenv is installed, this may cause issues', Colors.YELLOW)
 
     def run_cmd(self, cmd: str, *args: Any, **kwargs: Any) -> subprocess.CompletedProcess[bytes]:
         """Run command safely (relatively lol)"""
@@ -133,31 +135,27 @@ class DipDupEnvironment:
                 check=True,
             )
         except subprocess.CalledProcessError as e:
-            fail(f'{cmd} failed: {e.cmd} {e.returncode}')
+            self._quiet or fail(f'{cmd} failed: {e.cmd} {e.returncode}')
+            raise
 
     def ensure_pipx(self) -> None:
-        if not sys.version.startswith('3.11'):
-            fail('DipDup requires Python 3.11')
-
         """Ensure pipx is installed for current user"""
         if self._commands.get('pipx'):
             return
 
+        if sys.prefix != sys.base_prefix:
+            fail("pipx can't be installed in virtualenv, run `deactivate` and try again")
+
         echo('Installing pipx')
-        if sys.base_prefix != sys.prefix:
-            self.run_cmd('python3.11', '-m', 'pip', 'install', '-q', 'pipx')
-        else:
-            self.run_cmd('python3.11', '-m', 'pip', 'install', '--user', '-q', 'pipx')
-        self.run_cmd('python3.11', '-m', 'pipx', 'ensurepath')
-        pipx_path = str(Path.home() / '.local' / 'bin')
-        os.environ['PATH'] = pipx_path + os.pathsep + os.environ['PATH']
-        self._commands['pipx'] = which('pipx')
+        self.run_cmd('python3', '-m', 'pip', 'install', '--user', '-q', 'pipx')
+        self.run_cmd('python3', '-m', 'pipx', 'ensurepath')
+        os.environ['PATH'] = os.environ['PATH'] + ':' + str(Path.home() / '.local' / 'bin')
+        os.execv(sys.executable, [sys.executable] + sys.argv)
 
 
 def install(
     quiet: bool,
     force: bool,
-    version: str | None,
     ref: str | None,
     path: str | None,
 ) -> None:
@@ -165,94 +163,66 @@ def install(
     if ref and path:
         fail('Specify either ref or path, not both')
 
-    if not any((version, ref, path)):
-        # FIXME: Temporary, remove when 7.0.0 is released
-        version = '7.0.0rc2'
-
     env = DipDupEnvironment()
-    env.prepare()
-    if not quiet:
-        env.print()
+    env.check()
 
     force_str = '--force' if force else ''
     pipx_packages = env._pipx_packages
     pipx_dipdup = 'dipdup' in pipx_packages
     pipx_datamodel_codegen = 'datamodel-code-generator' in pipx_packages
-    pipx_pdm = 'pdm' in pipx_packages
+    pipx_poetry = 'poetry' in pipx_packages
 
-    python_inter_pipx = cast(str, which('python3.11'))
-    if 'pyenv' in python_inter_pipx:
-        python_inter_pipx = (
-            subprocess.run(['pyenv', 'which', 'python3.11'], capture_output=True, text=True)
-            .stdout.strip()
-            .split('\n')[0]
-        )
-
-    if pipx_dipdup and not force:
+    if pipx_dipdup:
         echo('Updating DipDup')
         env.run_cmd('pipx', 'upgrade', 'dipdup', force_str)
     else:
         if path:
             echo(f'Installing DipDup from `{path}`')
-            env.run_cmd('pipx', 'install', '--python', python_inter_pipx, path, force_str)
+            env.run_cmd('pipx', 'install', path, force_str)
         elif ref:
-            url = f'git+{GITHUB}@{ref}'
-            echo(f'Installing DipDup from `{url}`')
-            env.run_cmd('pipx', 'install', '--python', python_inter_pipx, url, force_str)
+            echo(f'Installing DipDup from `{ref}`')
+            env.run_cmd('pipx', 'install', f'git+{GITHUB}@{ref}', force_str)
         else:
             echo('Installing DipDup from PyPI')
-            pkg = 'dipdup' if not version else f'dipdup=={version}'
-            env.run_cmd('pipx', 'install', '--python', python_inter_pipx, pkg, force_str)
+            env.run_cmd('pipx', 'install', 'dipdup', force_str)
 
     if pipx_datamodel_codegen:
         env.run_cmd('pipx', 'upgrade', 'datamodel-code-generator', force_str)
     else:
-        env.run_cmd('pipx', 'install', '--python', python_inter_pipx, 'datamodel-code-generator', force_str)
+        env.run_cmd('pipx', 'install', 'datamodel-code-generator', force_str)
 
-    if pipx_pdm:
-        echo('Updating PDM')
-        env.run_cmd('pipx', 'upgrade', 'pdm', force_str)
-    elif quiet or ask('Install PDM? (recommended)', True):
-        echo('Installing PDM')
-        env.run_cmd('pipx', 'install', '--python', python_inter_pipx, 'pdm', force_str)
-        env._commands['pdm'] = which('pdm')
-        pipx_pdm = True
+    if (legacy_poetry := Path(Path.home(), '.poetry')).exists():
+        rmtree(legacy_poetry, ignore_errors=True)
+        env.run_cmd('pipx', 'install', 'poetry', force_str)
+    elif pipx_poetry:
+        echo('Updating Poetry')
+        env.run_cmd('pipx', 'upgrade', 'poetry', force_str)
+    elif ask('Install poetry? Optional for `dipdup new` command', True, quiet):
+        echo('Installing poetry')
+        env.run_cmd('pipx', 'install', 'poetry', force_str)
+        env._commands['poetry'] = which('poetry')
+        pipx_poetry = True
 
     done(
-        'Done! DipDup is ready to use.\nRun `dipdup new` to create a new project or `dipdup` to see all available'
-        ' commands.'
+        'Done! DipDup is ready to use.\nRun `dipdup new` to create a new project or `dipdup` to see all available commands.'
     )
-
-
-def ask(question: str, default: bool) -> bool:
-    """Ask user a yes/no question"""
-    while True:
-        answer = input(question + (' [Y/n] ' if default else ' [y/N] ')).lower().strip()
-        if not answer:
-            return default
-        return answer in ('n', 'no') if default else answer in ('y', 'yes')
 
 
 def uninstall(quiet: bool) -> NoReturn:
     """Uninstall DipDup and its dependencies with pipx"""
     env = DipDupEnvironment()
-    env.prepare()
-    if not quiet:
-        env.print()
+    env.check()
 
-    packages = (
-        ('dipdup', True),
-        ('datamodel-code-generator', False),
-        ('pdm', False),
-    )
-    for package, default in packages:
-        if package not in env._pipx_packages:
-            continue
-        if not quiet and not ask(f'Uninstall {package}?', default):
-            continue
+    pipx_packages = env._pipx_packages
 
-        echo(f'Uninstalling {package}')
-        env.run_cmd('pipx', 'uninstall', package)
+    if 'dipdup' in pipx_packages:
+        echo('Uninstalling DipDup')
+        env.run_cmd('pipx', 'uninstall', 'dipdup')
+
+    if 'datamodel-code-generator' in pipx_packages:
+        if ask('Uninstall datamodel-code-generator?', True, quiet):
+            echo('Uninstalling datamodel-code-generator')
+            env.run_cmd('pipx', 'uninstall', 'datamodel-code-generator')
 
     done('Done! DipDup is uninstalled.')
 
@@ -263,7 +233,6 @@ def cli() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('-q', '--quiet', action='store_true', help='Use default answers for all questions')
     parser.add_argument('-f', '--force', action='store_true', help='Force reinstall')
-    parser.add_argument('-v', '--version', help='Install DipDup from a specific version')
     parser.add_argument('-r', '--ref', help='Install DipDup from a specific git ref')
     parser.add_argument('-p', '--path', help='Install DipDup from a local path')
     parser.add_argument('-u', '--uninstall', action='store_true', help='Uninstall DipDup')
@@ -275,7 +244,6 @@ def cli() -> None:
         install(
             quiet=args.quiet,
             force=args.force,
-            version=args.version.strip() if args.version else None,
             ref=args.ref.strip() if args.ref else None,
             path=args.path.strip() if args.path else None,
         )
