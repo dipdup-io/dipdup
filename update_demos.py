d31e3235676ac6884053477906c9ceb4c8fdd0a1
droserasprout <droserasprout@users.noreply.github.com>
2023-08-02 12:22:44 +0000

    deploy: 75d8f0de6e1e013400054cf1a64f59ec599f6945

--- update_demos.py
+++ update_demos.py
@@ -3,11 +3,10 @@ import subprocess
 from pathlib import Path
 from shutil import rmtree
 
-from dipdup.project import answers_from_replay
-from dipdup.project import render_project
+from dipdup.project import BaseProject
 
 projects_path = Path(__file__).parent.parent / 'projects'
-demos_path = Path(__file__).parent.parent / 'src'
+demos_path = Path(__file__).parent.parent / 'demos'
 
 
 def _get_demos() -> list[Path]:
@@ -18,24 +17,41 @@ def _get_projects() -> list[Path]:
     return list(projects_path.iterdir())
 
 
-for path in _get_demos():
-    if not path.name.startswith('demo_'):
-        continue
-    if path.is_dir():
-        print(f'=> Removing `{path.name}`')
-        rmtree(path, ignore_errors=True)
-        rmtree(path.parent / 'src' / path.name, ignore_errors=True)
+for demo_path in _get_demos():
+    if demo_path.is_dir():
+        print(f'=> Removing `{demo_path.name}`')
+        rmtree(demo_path, ignore_errors=True)
+        rmtree(demo_path.parent / 'src' / demo_path.name, ignore_errors=True)
 
-for path in _get_projects():
-    package = path.name
-    if not package.startswith('demo_'):
+for project_path in _get_projects():
+    if not project_path.name.endswith('.json'):
         continue
 
-    print(f'=> Rendering {path}')
-    answers = answers_from_replay(path / 'replay.yaml')
-    answers['package'] = package
-    answers['template'] = package
+    print(f'=> Updating {project_path.name}')
+    project = BaseProject()
+    project.run(quiet=True, replay=str(project_path))
+    project.render(force=True)
 
-    render_project(answers, force=True)
+    project_name = project.answers['project_name']
+    package = project.answers['package']
+    subprocess.run(['mv', project_name, 'demos'], check=True)
 
-    subprocess.run(['mv', package, 'src'], check=True)
+for demo in _get_demos():
+    if not demo.is_dir():
+        continue
+
+    print(f'=> Initializing `{demo.name}`')
+    subprocess.run(
+        ['dipdup', 'init', '--overwrite-types'],
+        cwd=demo,
+        check=True,
+    )
+
+    demo_pkg = demo.name.replace('-', '_')
+    args = ['ln', '-sf', f'../demos/{demo.name}/src/{demo_pkg}', demo_pkg]
+    print(f'=> Linking `{demo.name}`: {" ".join(args)}')
+    subprocess.run(
+        args,
+        cwd=Path(__file__).parent.parent / 'src',
+        check=True,
+    )
