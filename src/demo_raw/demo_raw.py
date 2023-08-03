2e80e03259cf9ea9806533a4ae2891e2115efb14
Lev Gorodetskiy <github@droserasprout.space>
2023-08-03 16:53:59 -0300

    Revert missed argument

--- src/demo_raw/demo_raw.py
+++ src/demo_raw/demo_raw.py
@@ -1,4 +1,4 @@
 import dipdup
 
 # NOTE: Allows to use the package as a working directory
-dipdup.workdir(__file__)
+dipdup.workdir(__file__, __name__)
--- src/dipdup/__init__.py
+++ src/dipdup/__init__.py
@@ -8,19 +8,14 @@ __version__ = importlib.metadata.version('dipdup')
 __spec_version__ = '2.0'
 
 
-def workdir(file: str) -> None:
+def workdir(file: str, name: str) -> None:
     """A little hack to use Python package as a working directory."""
     import sys
     from importlib.util import module_from_spec
     from importlib.util import spec_from_file_location
     from pathlib import Path
 
-    name = Path(file).stem
     package_root = Path(file).parent / '__init__.py'
-    spec = spec_from_file_location(name, package_root)
-    if spec is None:
-        package_root.write_text('')
-
     spec = spec_from_file_location(name, package_root)
     if not spec or not spec.loader:
         raise ImportError(f'`{name}` is not a valid DipDup package')
