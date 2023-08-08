7e0fcc00019617997f998c06042cb6cc30390e17
Igor Sereda <sereda.igor.s@gmail.com>
2023-08-08 12:09:00 +0300

    Debug with timescaledb.continuous

--- src/demo_uniswap/hooks/on_reindex.py
+++ src/demo_uniswap/hooks/on_reindex.py
@@ -6,4 +6,5 @@ async def on_reindex(
     ctx: HookContext,
 ) -> None:
     # NOTE: Positions table is immune to decrease node usage, but we need to reset counters
+    await ctx.execute_sql('on_reindex')
     await models.Position.reset()
