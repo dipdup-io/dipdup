e1b4cbe1257c0f93569c72125817718bcc841c05
Igor Sereda <sereda.igor.s@gmail.com>
2023-11-06 16:28:02 +0300

    Alter hypertable chunk_time_interval

--- src/demo_uniswap/sql/on_reindex/00_prepare_db.sql
+++ src/demo_uniswap/sql/on_reindex/00_prepare_db.sql
@@ -2,4 +2,4 @@ CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 
 ALTER TABLE swap DROP CONSTRAINT swap_pkey;
 ALTER TABLE swap ADD PRIMARY KEY (id, timestamp);
-SELECT create_hypertable('swap', 'timestamp', chunk_time_interval => 2592000);
+SELECT create_hypertable('swap', 'timestamp');
