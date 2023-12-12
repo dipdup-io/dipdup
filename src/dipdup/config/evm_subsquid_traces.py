4d81f19755a0be6bb25cd2808d000caae5e49fcc
Lev Gorodetskiy <github@droserasprout.space>
2023-12-12 14:18:34 -0300

    Fix new configs for mypy to pass

--- src/dipdup/config/evm_subsquid_traces.py
+++ src/dipdup/config/evm_subsquid_traces.py
@@ -1,16 +1,28 @@
 from __future__ import annotations
 
+from dataclasses import field
 from typing import Literal
 
 from pydantic.dataclasses import dataclass
 
+from dipdup.config import AbiDatasourceConfig
+from dipdup.config import HandlerConfig
 from dipdup.config import IndexConfig
 from dipdup.config.evm_subsquid import SubsquidDatasourceConfig
 
 
 @dataclass
+class SubsquidTracesHandlerConfig(HandlerConfig): ...
+
+
+@dataclass
 class SubsquidTracesIndexConfig(IndexConfig):
     kind: Literal['evm.subsquid.traces']
-    datasource: SubsquidDatasourceConfig
 
-    ...
+    datasource: SubsquidDatasourceConfig
+    handlers: tuple[SubsquidTracesHandlerConfig, ...] = field(default_factory=tuple)
+    abi: AbiDatasourceConfig | tuple[AbiDatasourceConfig, ...] | None = None
+    node_only: bool = False
+
+    first_level: int = 0
+    last_level: int = 0
--- src/dipdup/config/evm_subsquid_transactions.py
+++ src/dipdup/config/evm_subsquid_transactions.py
@@ -1,16 +1,28 @@
 from __future__ import annotations
 
+from dataclasses import field
 from typing import Literal
 
 from pydantic.dataclasses import dataclass
 
+from dipdup.config import AbiDatasourceConfig
+from dipdup.config import HandlerConfig
 from dipdup.config import IndexConfig
 from dipdup.config.evm_subsquid import SubsquidDatasourceConfig
 
 
 @dataclass
+class SubsquidTransactionsHandlerConfig(HandlerConfig): ...
+
+
+@dataclass
 class SubsquidTransactionsIndexConfig(IndexConfig):
     kind: Literal['evm.subsquid.transactions']
-    datasource: SubsquidDatasourceConfig
 
-    ...
+    datasource: SubsquidDatasourceConfig
+    handlers: tuple[SubsquidTransactionsHandlerConfig, ...] = field(default_factory=tuple)
+    abi: AbiDatasourceConfig | tuple[AbiDatasourceConfig, ...] | None = None
+    node_only: bool = False
+
+    first_level: int = 0
+    last_level: int = 0
