0de411c27e8591ad7a487b5ae219fbb6b6a83ca9
Lev Gorodetskiy <github@droserasprout.space>
2024-02-15 18:00:53 -0300

    notify about EOL

--- src/dipdup/cli.py
+++ src/dipdup/cli.py
@@ -171,7 +171,7 @@ async def cli(ctx: click.Context, config: list[str], env_file: list[str]) -> Non
 
     # NOTE: Fire and forget, do not block instant commands
     if not any((_config.advanced.skip_version_check, env.TEST, env.CI)):
-        _logger.info('DipDup 7.0 is out! Visit https://dipdup.io/docs to learn more.')
+        _logger.warning('WARNING: DipDup 6.5 is no longer supported. Visit https://dipdup.io/docs/release-notes/v7.0#migration-guide')
         # asyncio.ensure_future(_check_version())
 
     # NOTE: Avoid import errors if project package is incomplete
