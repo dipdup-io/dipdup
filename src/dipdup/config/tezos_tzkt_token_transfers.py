80a39c7f5e48f30896a9d9d1cb1061b0c8b502ec
Lev Gorodetskiy <github@droserasprout.space>
2024-04-08 10:33:46 -0300

    Fixed "from" fields in config

--- src/dipdup/config/tezos_tzkt_token_transfers.py
+++ src/dipdup/config/tezos_tzkt_token_transfers.py
@@ -34,8 +34,7 @@ class TzktTokenTransfersHandlerConfig(HandlerConfig):
 
     contract: TezosContractConfig | None = None
     token_id: int | None = None
-    # FIXME: Can't use `from_` field alias in dataclass
-    # FIXME: See https://github.com/pydantic/pydantic/issues/4286 (fixed in upcoming v2)
+    # FIXME: Can't use `from_` field alias in dataclasses (fixed in `next` with Pydantic v2)
     from_: TezosContractConfig | None = None
     to: TezosContractConfig | None = None
 
