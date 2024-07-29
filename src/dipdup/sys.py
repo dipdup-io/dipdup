0c4e12d49f01ea5e72242d5c83296e39438a92b2
Lev Gorodetskii <github@droserasprout.space>
2024-07-29 12:28:12 -0300

    Use `uvloop` to improve asyncio performance (#1074)

--- src/dipdup/sys.py
+++ src/dipdup/sys.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-import signal
 import sys
 import warnings
 from collections import deque
@@ -13,26 +12,7 @@ from pydantic_core import to_jsonable_python
 
 from dipdup import env
 
-_is_shutting_down = False
 _futures: deque[asyncio.Future[None]] = deque()
-
-
-async def _shutdown() -> None:  # pragma: no cover
-    global _is_shutting_down
-    if _is_shutting_down:
-        return
-    _is_shutting_down = True
-
-    # NOTE: Prevents BrokenPipeError when piping output to another process
-    # sys.stderr.close()
-
-    tasks = filter(lambda t: t != asyncio.current_task(), asyncio.all_tasks())
-    list(map(asyncio.Task.cancel, tasks))
-    await asyncio.gather(*tasks, return_exceptions=True)
-
-
-def is_shutting_down() -> bool:
-    return _is_shutting_down
 
 
 def set_up_logging() -> None:
@@ -68,19 +48,11 @@ def fire_and_forget(aw: Awaitable[Any]) -> None:
     future.add_done_callback(lambda _: _futures.remove(future))
 
 
-def set_up_process(signals: bool) -> None:
+def set_up_process() -> None:
     """Set up interpreter process-wide state"""
     # NOTE: Skip for integration tests
     if env.TEST:
         return
-
-    # NOTE: Register shutdown handler for non-interactive commands (avoiding conflicts with Click prompts)
-    if signals:
-        loop = asyncio.get_running_loop()
-        loop.add_signal_handler(
-            signal.SIGINT,
-            lambda: asyncio.ensure_future(_shutdown()),
-        )
 
     # NOTE: Better discoverability of DipDup packages and configs
     sys.path.append(str(Path.cwd()))
