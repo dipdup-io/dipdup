74d06cb72d579b388d822dd0a5e8d788c43c3dd3
Lev Gorodetskiy <github@droserasprout.space>
2025-04-12 15:20:45 -0300

    Refactor test configs

--- tests/test_dipdup.py
+++ tests/test_dipdup.py
@@ -31,7 +31,7 @@ async def _create_index(hash_: str) -> None:
 
 class IndexStateTest:
     def __init__(self) -> None:
-        name = 'demo_tezos_nft_marketplace.yml'
+        name = 'demo_tezos_nft_marketplace.yaml'
         config_path = Path(__file__).parent / 'configs' / name
         self.config = DipDupConfig.load([config_path])
         self.config.database = SqliteDatabaseConfig(kind='sqlite')
