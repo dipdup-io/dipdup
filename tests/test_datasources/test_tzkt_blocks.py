1f09c77144490032cd27682a820e1d88177bf384
Lev Gorodetskiy <github@droserasprout.space>
2025-08-06 15:25:15 -0300

    Update TzKT models for Seoulnet

--- tests/test_datasources/test_tzkt_blocks.py
+++ tests/test_datasources/test_tzkt_blocks.py
@@ -1,5 +1,4 @@
-import json
-
+import orjson
 import pytest
 
 from dipdup.models.tezos import TezosBlockData
@@ -12,7 +11,7 @@ from dipdup.models.tezos import TezosBlockData
     ],
 )
 async def test_deprecated_priority(tzkt_block_json: str) -> None:
-    tzkt_block_dict = json.loads(tzkt_block_json)
+    tzkt_block_dict = orjson.loads(tzkt_block_json)
     block = TezosBlockData.from_json(tzkt_block_dict)
     assert block
     assert isinstance(block, TezosBlockData)
@@ -24,3 +23,63 @@ async def test_deprecated_priority(tzkt_block_json: str) -> None:
     assert block
     assert isinstance(block, TezosBlockData)
     assert block.priority is None
+
+
+async def test_seoulnet_response() -> None:
+    from dipdup.models.tezos import TezosBlockData
+    from dipdup.models.tezos import TezosHeadBlockData
+
+    tzkt_head_json = {
+        'chain': 'ghostnet',
+        'chainId': 'NetXnHfVqm9iesp',
+        'cycle': 1674,
+        'level': 14197527,
+        'hash': 'BKqV7LnQfrfngY2JqA6Rn4pgXPho5BZPpuJcNLYHNfaLsyeFLoC',
+        'protocol': 'PsRiotumaAMotcRoDWW1bysEhQy2n1M5fy8JgRp8jjRfHGmfeA7',
+        'nextProtocol': 'PsRiotumaAMotcRoDWW1bysEhQy2n1M5fy8JgRp8jjRfHGmfeA7',
+        'timestamp': '2025-08-06T15:38:00Z',
+        'votingEpoch': 1326,
+        'votingPeriod': 1329,
+        'knownLevel': 14197527,
+        'lastSync': '2025-08-06T15:38:01Z',
+        'synced': True,
+        'quoteLevel': 14197527,
+        'quoteBtc': 6.76257481962031e-06,
+        'quoteEur': 0.6684812122856512,
+        'quoteUsd': 0.7780356315250635,
+        'quoteCny': 5.588552137681382,
+        'quoteJpy': 114.6411710177945,
+        'quoteKrw': 1078.1838981305855,
+        'quoteEth': 0.00021434607004505848,
+        'quoteGbp': 0.5830949138683016,
+    }
+    tzkt_block_json = {
+        'cycle': 1674,
+        'level': 14197527,
+        'hash': 'BKqV7LnQfrfngY2JqA6Rn4pgXPho5BZPpuJcNLYHNfaLsyeFLoC',
+        'timestamp': '2025-08-06T15:38:00Z',
+        'proto': 12,
+        'payloadRound': 0,
+        'blockRound': 0,
+        'validations': 6878,
+        'deposit': 0,
+        'rewardDelegated': 70525,
+        'rewardStakedOwn': 48207330,
+        'rewardStakedEdge': 0,
+        'rewardStakedShared': 0,
+        'bonusDelegated': 66787,
+        'bonusStakedOwn': 45652271,
+        'bonusStakedEdge': 0,
+        'bonusStakedShared': 0,
+        'fees': 1502,
+        'nonceRevealed': False,
+        'proposer': {'alias': 'Marco S', 'address': 'tz1Zt8QQ9aBznYNk5LUBjtME9DuExomw9YRs'},
+        'producer': {'alias': 'Marco S', 'address': 'tz1Zt8QQ9aBznYNk5LUBjtME9DuExomw9YRs'},
+        'software': {'version': 'v22.0', 'date': '2025-04-07T11:33:08Z'},
+        'lbToggle': True,
+        'lbToggleEma': 1999,
+        'aiToggleEma': 1788871656,
+    }
+
+    TezosHeadBlockData.from_json(tzkt_head_json)
+    TezosBlockData.from_json(tzkt_block_json)
