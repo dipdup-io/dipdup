df75583ac7a475773a814479708a59ab72888eff
Wizard1209 <wizard1209@pm.me>
2025-10-20 11:26:56 -0300

    revert

--- src/dipdup/indexes/tezos_operations/parser.py
+++ src/dipdup/indexes/tezos_operations/parser.py
@@ -27,8 +27,8 @@ def extract_root_outer_type(storage_type: type[BaseModel]) -> type[BaseModel]:
     """Extract Pydantic __root__ type"""
     root_field = storage_type.model_fields['root']
     if not root_field.is_required():
-        # NOTE: Optional is a magic _SpecialForm
-        return cast('type[BaseModel]', Optional[root_field.annotation])  # noqa: UP007
+        # NOTE: Optional is a magic _SpecialForm that handles None/Union edge cases
+        return cast('type[BaseModel]', Optional[root_field.annotation])  # noqa: UP045
 
     return root_field.annotation  # type: ignore[return-value]
 
