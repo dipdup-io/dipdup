3a6876a5fb645b2a656a15af29728759f9b4c9a1
Ocheretovich Oksana <ocheretovich@gmail.com>
2026-03-03 11:56:35 +0200

    fix(context): execute pending hooks in _hooks_loop
    Signed-off-by: Ocheretovich Oksana <ocheretovich@gmail.com>

--- src/dipdup/context.py
+++ src/dipdup/context.py
@@ -610,7 +610,8 @@ class DipDupContext:
 
     async def _hooks_loop(self) -> None:
         while True:
-            await self._pending_hooks.get()
+            coro = await self._pending_hooks.get()
+            await coro
 
     def register_handler(self, handler_config: HandlerConfig) -> None:
         # NOTE: Same handlers can be linked to different indexes, we need to use exact config
